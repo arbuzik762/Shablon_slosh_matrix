6dd1f5294a4ce390cf8e5d49fd7b06f3539fb942
arbuzik762 <arbuzik2005maks@gmail.com>
2025-04-25 08:22:38 +0300

    Update sloshenie.cpp

--- sloshenie.cpp
+++ sloshenie.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int** matrixAddition(int** matrix1, int** matrix2, int rows, int cols) {
+int** sloshenie_matrix(int** m_1, int** m_2, int rows, int cols) {
     int** result = (int**)malloc(rows * sizeof(int*));
     for (int i = 0; i < rows; i++) {
         result[i] = (int*)malloc(cols * sizeof(int));
         for (int j = 0; j < cols; j++) {
-            result[i][j] = matrix1[i][j] + matrix2[i][j];
+            result[i][j] = m_1[i][j] + m_2[i][j];
         }
     }
     return result;
 }
 
-void freeMatrix(int** matrix, int rows) {
+void Matrix(int** matrix, int rows) {
     for (int i = 0; i < rows; i++) {
         free(matrix[i]);
     }
@@ -33,9 +33,9 @@ int main() {
         }
     }
 
-    int** C = matrixAddition(A, B, rows, cols);
+    int** C = sloshenie_matrix(A, B, rows, cols);
 
-    printf("Результат сложения:\n");
+    printf("Результат:\n");
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             printf("%d ", C[i][j]);
@@ -43,9 +43,9 @@ int main() {
         printf("\n");
     }
 
-    freeMatrix(A, rows);
-    freeMatrix(B, rows);
-    freeMatrix(C, rows);
+    Matrix(A, rows);
+    Matrix(B, rows);
+    Matrix(C, rows);
 
     return 0;
 }
